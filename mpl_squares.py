482a8e83bc8e005591446038ea438d66e672f83b
Konstantin Alimov <alimovshared@gmail.com>
2026-09-12 11:18:00 +0300

    Simple line plot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,4 +1,13 @@
 import matplotlib.pyplot as plt
-squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+
+input_values = [1, 2, 3, 4, 5, 6, 7, 8]
+squares = [1, 4, 9, 16, 25, 36, 49, 64]
+plt.plot(input_values, squares, linewidth=5)
+
+# Назначение заголовка диаграммы и меток осей.
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+# Назначение размера шрифта делений на осях.
+plt.tick_params(axis='both', labelsize=14)
 plt.show()
