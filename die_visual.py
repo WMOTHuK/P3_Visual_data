042b0f3c03a438407f720663e3fdb3d1b785c3a9
Konstantin Alimov <alimovshared@gmail.com>
2026-09-13 09:44:29 +0300

    single die rolls

---
+++ die_visual.py
@@ -0,0 +1,29 @@
+from die import Die
+import pygal
+# Создание кубика D6.
+die = Die()
+# Моделирование серии бросков с сохранением результатов в списке.
+results = []
+numb_rolls = 100
+for roll_num in range(numb_rolls):
+    result = die.roll()
+    results.append(result)
+
+# Анализ результатов.
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Визуализация результатов.
+hist = pygal.Bar()
+
+
+hist.title = f"Results of rolling one D6 {numb_rolls} times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
+
